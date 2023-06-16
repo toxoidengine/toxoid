d0d3a9a2f46d06a5f2d195227c19c3b3f4f030b5
troyedwardsjr <troyedwardsjr@gmail.com>
2023-06-15 21:25:40 -0400

    register tag

--- examples/snake/snake/src/lib.rs
+++ examples/snake/snake/src/lib.rs
@@ -1,8 +1,38 @@
+#![allow(non_camel_case_types)]
+pub type ecs_id_t = i32;
+pub type ecs_entity_t = ecs_id_t;
+pub type c_char = u8;
+
 extern "C" {
-    pub fn sephiroth(a: i32, b: i32) -> i32;
+    pub fn toxoid_print_i32(v: i32);
+    pub fn toxoid_print_string(v: *const i8);
+    pub fn toxoid_register_tag(name: *const i8, name_len: usize) -> ecs_entity_t;
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn app_main() {
-    sephiroth(5, 6);
+    let tag = register_tag("PositionTag");
+    let tag_2 = register_tag("PositionTagTwo");
+    print_i32(tag);
+    print_i32(tag_2);
+}
+
+pub fn print_i32(v: i32) {
+    unsafe {
+        toxoid_print_i32(v);
+    }
+}
+
+pub fn print_string(v: &str) {
+    unsafe {
+        toxoid_print_string(v.as_bytes().as_ptr() as *const i8);
+    }
+}
+
+pub fn register_component() {}
+
+pub fn register_tag(name: &str) -> ecs_entity_t {
+    unsafe {
+        toxoid_register_tag(name.as_bytes().as_ptr() as *const i8, name.len())
+    }
 }
--- examples/snake/snake_engine/src/main.rs
+++ examples/snake/snake_engine/src/main.rs
@@ -3,10 +3,26 @@ extern "C" {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sephiroth(a: i32, b: i32) -> i32 {
-    println!("Printing from Toxoid Engine! {:?}", a + b);
-    a + b
+pub unsafe extern "C" fn toxoid_print_i32(v: i32) {
+    println!("Printing from Toxoid Engine: {}", v);
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn toxoid_print_string(v: *const i8) {
+    let v = unsafe { std::ffi::CStr::from_ptr(v as *const i8) };
+    let v = v.to_string_lossy().into_owned();
+    println!("Printing from Toxoid Engine: {}", v);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn toxoid_register_tag(name: *const i8, name_len: usize) -> i32 {
+    let slice = std::slice::from_raw_parts(name as *mut u8, name_len);
+    let string = std::str::from_utf8_unchecked(slice);
+    println!("Created tag named: {}", string);
+    flecs_core::flecs_tag_create(name) as i32
+}
+
+
 
 #[no_mangle]
 pub unsafe extern "C" fn app_init() {
