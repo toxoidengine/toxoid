3341cb677e5acc9ba7f8e768b6ca7357c6fb0e94
troyedwardsjr <troyedwardsjr@gmail.com>
2024-03-10 01:36:09 -0500

    remove thread id printing

--- examples/snake/snake_engine/src/main.rs
+++ examples/snake/snake_engine/src/main.rs
@@ -1,4 +1,3 @@
-#![feature(thread_id_value)]
 #![allow(improper_ctypes_definitions)]
 extern "C" {
     // Main function of the dynamically linked library / Toxoid App.
@@ -6,11 +5,6 @@ extern "C" {
 }
 
 fn main() {
-    println!("This runs in the main thread: {}", std::thread::ThreadId::as_u64(&std::thread::current().id()));
-    std::thread::spawn(|| {
-        println!("This runs in a new thread: {}", std::thread::ThreadId::as_u64(&std::thread::current().id()));
-    });
-
     // Initialize Toxoid ECS initializers + default components + default sy stems.
     toxoid_engine::init();
 }
